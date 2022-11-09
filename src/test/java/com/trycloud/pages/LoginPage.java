27a39888eb67d76533864295793be2c862d5b9ba
Maria <kitmaria.2015@gmail.com>
2022-11-09 21:31:00 +0100

    located elements on LoginPage login method created

--- src/test/java/com/trycloud/pages/LoginPage.java
+++ src/test/java/com/trycloud/pages/LoginPage.java
@@ -12,31 +12,35 @@ public class LoginPage {
         PageFactory.initElements(Driver.getDriver(), this);
     }
 
-// don't forget to add not just elements here
-    // but also the login method
-    @FindBy(id = "user")
-    public WebElement userNameBox;
+    @FindBy(id="user")
+    public WebElement inputUsername;
 
-    @FindBy(xpath = "//input[@name='password']")
-    public WebElement passwordBox;
+    @FindBy(id="password")
+    public WebElement inputPassword;
 
-    @FindBy(xpath = "//input[@value = 'Log in']")
+    @FindBy(id="submit-form")
     public WebElement loginButton;
 
-   public void login(String userName, String password){
-        userNameBox.sendKeys(userName);
-        passwordBox.sendKeys(password);
+   public void login1(String userName, String password){
+        inputUsername.sendKeys(userName);
+        inputPassword.sendKeys(password);
         loginButton.click();
     }
 
+    @FindBy(xpath = "//p[@class='warning wrongPasswordMsg']")
+    public WebElement message;
 
- }
+    public void login (String username, String password){
+        inputUsername.sendKeys(username);
+        inputPassword.sendKeys(password);
+    }
+    public void login (){
+        inputUsername.sendKeys(ConfigReader.getProperty("username3"));
+        inputPassword.sendKeys(ConfigReader.getProperty("password"));
+    }
+    public void goToLoginPage(){
+        Driver.getDriver().get(ConfigReader.getProperty("login"));
+    }
 
 
-
-
-
-
-
-
-
+}
