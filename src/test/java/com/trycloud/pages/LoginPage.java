bde3f367c34a277a5440f2fb818967c5122971cb
Maria <kitmaria.2015@gmail.com>
2022-11-09 21:31:00 +0100

    located elements on LoginPage login method created

--- src/test/java/com/trycloud/pages/LoginPage.java
+++ src/test/java/com/trycloud/pages/LoginPage.java
@@ -1,6 +1,9 @@
 package com.trycloud.pages;
 
+import com.trycloud.utilities.ConfigReader;
 import com.trycloud.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage {
@@ -9,14 +12,29 @@ public class LoginPage {
         PageFactory.initElements(Driver.getDriver(), this);
     }
 
-// don't forget to add not just elements here
-    // but also the login method
+    @FindBy(id="user")
+    public WebElement inputUsername;
 
+    @FindBy(id="password")
+    public WebElement inputPassword;
 
+    @FindBy(id="submit-form")
+    public WebElement loginButton;
 
+    @FindBy(xpath = "//p[@class='warning wrongPasswordMsg']")
+    public WebElement message;
 
-
-
+    public void login (String username, String password){
+        inputUsername.sendKeys(username);
+        inputPassword.sendKeys(password);
+    }
+    public void login (){
+        inputUsername.sendKeys(ConfigReader.getProperty("username3"));
+        inputPassword.sendKeys(ConfigReader.getProperty("password"));
+    }
+    public void goToLoginPage(){
+        Driver.getDriver().get(ConfigReader.getProperty("login"));
+    }
 
 
 }
