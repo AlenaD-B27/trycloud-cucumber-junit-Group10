d63f65e69733592e7c868ca93bc9125b66eee23b
Maria <kitmaria.2015@gmail.com>
2022-11-09 21:37:13 +0100

    LoginStepDefinition created

---
+++ src/test/java/com/trycloud/step_definitions/LoginValidStepDefinitions.java
@@ -0,0 +1,31 @@
+package com.trycloud.step_definitions;
+
+import com.trycloud.pages.LoginPage;
+import com.trycloud.utilities.ConfigReader;
+import com.trycloud.utilities.Driver;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+
+public class LoginValidStepDefinitions {
+    LoginPage loginPage = new LoginPage();
+    @Given("user on the login page")
+    public void userOnTheLoginPage() {
+        loginPage.goToLoginPage();
+    }
+    @When("user enter username {string} and passcode {string}")
+    public void user_enter_username_and_passcode(String username, String password) {
+        loginPage.login(username,password);
+    }
+    @When("user click the login button")
+    public void user_click_the_login_button() {
+        loginPage.loginButton.click();
+    }
+    @Then("Verify the user should be at the dashboard page")
+    public void verify_the_user_should_be_at_the_dashboard_page() {
+        System.out.println("Driver.getDriver().getCurrentUrl() = " + Driver.getDriver().getCurrentUrl());
+    }
+
+    }
+
