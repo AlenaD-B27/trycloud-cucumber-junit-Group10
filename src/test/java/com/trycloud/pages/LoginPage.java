a87bf9550fc983cc8646e4265456bf708ac4bac6
AlenaD-B27 <lenador.18@gmail.com>
2022-11-08 21:57:11 -0600

    Login method and elements are added

--- src/test/java/com/trycloud/pages/LoginPage.java
+++ src/test/java/com/trycloud/pages/LoginPage.java
@@ -1,6 +1,9 @@
 package com.trycloud.pages;
 
+import com.trycloud.utilities.ConfigReader;
 import com.trycloud.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage {
@@ -9,8 +12,23 @@ public class LoginPage {
         PageFactory.initElements(Driver.getDriver(), this);
     }
 
-// don't forget to add not just elements here
-    // but also the login method
+
+
+    @FindBy(css = "input#user")
+    public WebElement inputUserBox;
+
+    @FindBy(css = "input#password")
+    public WebElement inputPasswordBox;
+
+    @FindBy(css = "input#submit-form")
+    public WebElement submitButton;
+
+
+    public void login(String username){
+        inputUserBox.sendKeys(username);
+        inputPasswordBox.sendKeys(ConfigReader.getProperty("password"));
+        submitButton.click();
+    }
 
 
 
