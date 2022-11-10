88cdd7e7d5902569022a72ce165704d2c6c5ef86
olhasoroka <olly0296@gmail.com>
2022-11-10 02:05:36 -0600

    hello.feature, LoginPage added some changes.

--- src/test/java/com/trycloud/pages/LoginPage.java
+++ src/test/java/com/trycloud/pages/LoginPage.java
@@ -1,6 +1,8 @@
 package com.trycloud.pages;
 
 import com.trycloud.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage {
@@ -11,6 +13,17 @@ public class LoginPage {
 
 // don't forget to add not just elements here
     // but also the login method
+    @FindBy(id = "user")
+    public WebElement userNameBox;
+
+    @FindBy(xpath = "//input[@name='password']")
+    public WebElement passwordBox;
+
+    @FindBy(xpath = "//input[@value = 'Log in']")
+    public WebElement loginButton;
+
+
+    }
 
 
 
@@ -19,4 +32,4 @@ public class LoginPage {
 
 
 
-}
+
