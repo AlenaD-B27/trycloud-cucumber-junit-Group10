05e2146cb1fbf34b844c70fa8ef0fac22304d9d2
olhasoroka <olly0296@gmail.com>
2022-11-10 02:04:47 -0600

    remove_files_to_favorite.feature file, RemoveFromFavPage, RemoveFromFavoriteStepDef are created.

---
+++ src/test/java/com/trycloud/pages/RemoveFromFavPage.java
@@ -0,0 +1,30 @@
+package com.trycloud.pages;
+
+import com.trycloud.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
+
+public class RemoveFromFavPage {
+
+    public RemoveFromFavPage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(className = "app-icon")
+    public WebElement filesButton;
+
+    @FindBy(xpath = "//*[.='Actions']")
+    public List<WebElement> actionsButtons;
+
+    @FindBy(xpath = "//*[.='Remove from favorites']")
+    public List<WebElement> removeFavorButton;
+
+    @FindBy(xpath = "//a[.='Favorites']")
+    public WebElement favoritesButton;
+
+
+
+}
