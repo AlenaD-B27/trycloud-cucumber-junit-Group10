8b9cc4c60a7cbd943209a19eb7fd3ed194f9b3bc
Maria <kitmaria.2015@gmail.com>
2022-11-13 22:19:40 +0100

    LoginInvalidStepDefinitions.java is created and snippets are added

---
+++ src/test/java/com/trycloud/step_definitions/LoginInvalidStepDefinitions.java
@@ -0,0 +1,16 @@
+package com.trycloud.step_definitions;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LoginInvalidStepDefinitions {
+    @When("user enter invalid {string} and {string}")
+    public void user_enter_invalid_and(String string, String string2) {
+
+    }
+    @Then("verify {string} message should be displayed")
+    public void verify_message_should_be_displayed(String string) {
+
+    }
+
+}
