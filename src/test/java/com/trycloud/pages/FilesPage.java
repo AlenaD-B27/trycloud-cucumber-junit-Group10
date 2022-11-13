4fedab264dbc8ced21b7a7f420289e4c279a594d
Elena Bagirgan <lena-40a@hotmail.com>
2022-11-13 14:59:13 -0500

    ElenaB: almost done with StepDef but have questions

--- src/test/java/com/trycloud/pages/FilesPage.java
+++ src/test/java/com/trycloud/pages/FilesPage.java
@@ -1,4 +1,107 @@
 package com.trycloud.pages;
 
-public class FilesPage {
+import com.trycloud.utilities.BrowserUtils;
+import com.trycloud.utilities.Driver;
+import org.junit.Assert;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import java.util.ArrayList;
+import java.util.List;
+public class FilesPage extends BasePage {
+    public FilesPage (){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+
+
+
+    @FindBy (xpath = "//span[text()='Files']")
+    public WebElement FilesIcon;
+
+
+
+    @FindBy (xpath = "//span[@class='icon icon-add']")
+    public WebElement addIconButton;
+
+    @FindBy (xpath = "//label[@data-action='upload']")
+    public WebElement uploadButton;
+
+
+
+    @FindBy (xpath = "//input[@type ='file']")  // id="uploadprogressbar"
+    public WebElement inputButton;
+
+    @FindBy(xpath = "//td[@class='filename ui-draggable ui-draggable-handle']")
+    public List<WebElement> listOfFiles;
+
+    public void listOfFiles(){
+        List<String> list = new ArrayList<>();
+        for(int i = 1; i < listOfFiles.size(); i++){
+            list.add(listOfFiles.get(i).getText());
+            System.out.println(listOfFiles.get(i).getText());
+        }
+        Assert.assertTrue(list.contains("ElenaB"));
+    }
+
+    @FindBy (xpath = "//span[.='New folder']")
+    public WebElement newFolder;
+
+    @FindBy (xpath = "//input[@id='view13-input-folder']")
+    public WebElement inputFolderName;
+
+    @FindBy (xpath = "(//input[@type='submit'])[2]")
+    public WebElement submitFolderName;
+
+    @FindBy (xpath = "//a[@class='filename']")
+    public WebElement newCreated;
+
+    @FindBy (xpath = "//div[@style='background-image:url(/index.php/apps/theming/img/core/filetypes/folder.svg?v=0);']")
+    public List<WebElement> folderType;
+
+
+    @FindBy (xpath = "//tr[@data-type='dir']//label")
+    public List<WebElement> folderCheckBox;
+
+    @FindBy (xpath = "//span[@class='innernametext']")
+    public List<WebElement> fileNameVerify;
+
+    @FindBy (xpath = "//tbody[@id='fileList']/tr")
+    public List<WebElement> fileType;
+
+
+    public void selectFolder(){
+        for(int i = 0; i < folderType.size(); i++){
+            if (folderType.get(i).isDisplayed()){
+                folderCheckBox.get(i).click();
+            }
+        }
+    }
+
+    public void verifyFileName(List<WebElement> listOfElements, String expected){
+        String actual = "";
+        for (WebElement each : listOfElements){
+            if(each.getText().equalsIgnoreCase(expected)){
+                actual += "" + each.getText();
+                break;
+            }
+        }
+        System.out.println(actual);
+        Assert.assertEquals(expected,actual);
+    }
+
+    public void chooseFolder(){
+        List<WebElement> list2 = new ArrayList<>();
+        for (int i = 1; i < fileType.size(); i++) {
+            BrowserUtils.hover(fileType.get(i));
+            if (fileType.get(i).getAttribute("data-type").equalsIgnoreCase("dir")) {
+                list2.add(fileType.get(i));
+            }
+        }
+        list2.get(1).click();
+    }
+
+
+
+
 }
--- src/test/java/com/trycloud/step_definitions/ManageFilesStepDef.java
+++ src/test/java/com/trycloud/step_definitions/ManageFilesStepDef.java
@@ -1,10 +1,26 @@
 package com.trycloud.step_definitions;
 
+
+import com.trycloud.pages.BasePage;
+import com.trycloud.pages.FilesPage;
+import com.trycloud.pages.LoginPage;
+import com.trycloud.utilities.BrowserUtils;
+import com.trycloud.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
+import com.github.javafaker.Faker;
 
-public class ManageFilesStepDef {
+
+public class ManageFilesStepDef  {
+
+    BasePage basePage = new BasePage();
+    LoginPage loginPage = new LoginPage();
+    FilesPage FilesPage = new FilesPage();
+
+
+
     @Given("user on the dashboard page")
     public void user_on_the_dashboard_page() {
         // Write code here that turns the phrase above into concrete actions
@@ -13,37 +29,50 @@ public class ManageFilesStepDef {
 
     @When("the user clicks the {string} module")
     public void the_user_clicks_the_module(String string) {
-        // Write code here that turns the phrase above into concrete actions
+
+      // String Files = FilesPage.FilesIcon.getText();
+        FilesPage.FilesIcon.click();
+
+
+
         throw new io.cucumber.java.PendingException();
     }
 
     @When("user clicks the add icon on the top")
     public void user_clicks_the_add_icon_on_the_top() {
-        // Write code here that turns the phrase above into concrete actions
+
+        FilesPage.addIconButton.click();
+        BrowserUtils.waitFor(1);
         throw new io.cucumber.java.PendingException();
     }
 
     @When("user click \"new folder”")
     public void user_click_new_folder() {
-        // Write code here that turns the phrase above into concrete actions
+
+        BrowserUtils.waitFor(1);
+        FilesPage.newFolder.click();
+        BrowserUtils.waitFor(1);
         throw new io.cucumber.java.PendingException();
     }
 
     @When("user write a folder name")
     public void user_write_a_folder_name() {
-        // Write code here that turns the phrase above into concrete actions
+        FilesPage.inputFolderName.sendKeys("ElenaB");
+        BrowserUtils.waitFor(1);
         throw new io.cucumber.java.PendingException();
     }
 
     @When("the user click submit icon")
     public void the_user_click_submit_icon() {
-        // Write code here that turns the phrase above into concrete actions
+        FilesPage.submitFolderName.click();
+        BrowserUtils.waitFor(3);
         throw new io.cucumber.java.PendingException();
     }
 
     @Then("Verify the folder is displayed on the page")
     public void verify_the_folder_is_displayed_on_the_page() {
-        // Write code here that turns the phrase above into concrete actions
+        Assert.assertTrue(FilesPage.newCreated.getText().contains("ElenaB"));
+        System.out.println(FilesPage.newCreated.getText());
         throw new io.cucumber.java.PendingException();
     }
 
