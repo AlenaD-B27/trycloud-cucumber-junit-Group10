4c885c2517db4979a5bcd3bd9491847344d2eb15
Vitalii <vtrushch@gmail.com>
2022-11-15 18:31:43 -0500

    create login step

--- src/test/java/com/trycloud/step_definitions/ViewContactStepDefinitions.java
+++ src/test/java/com/trycloud/step_definitions/ViewContactStepDefinitions.java
@@ -1,14 +1,23 @@
 package com.trycloud.step_definitions;
 
+import com.trycloud.pages.LoginPage;
+import com.trycloud.utilities.ConfigReader;
+import com.trycloud.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class ViewContactStepDefinitions {
-    @Given("{string} on the dashboard page")
-    public void onTheDashboardPage(String arg0) {
 
+    LoginPage loginPage = new LoginPage();
+    @Given("{string} on the dashboard page")
+    public void onTheDashboardPage(String user) {
+        Driver.getDriver().get(ConfigReader.getProperty("env"));
+        loginPage.inputUsername.sendKeys(user);
+        loginPage.inputPassword.sendKeys(ConfigReader.getProperty("password"));
+        loginPage.loginButton.click();
     }
+
     @When("the user clicks the contacts module")
     public void the_user_clicks_the_contacts_module() {
 
@@ -17,6 +26,7 @@ public class ViewContactStepDefinitions {
     public void verify_the_contact_names_are_in_the_list() {
 
     }
+
 
 
 }
