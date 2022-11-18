820ce94fcae793d726d0f8c93dcecc1e341d1db8
Olesya-Talanina <talaninaolesya@gmail.com>
2022-11-18 12:44:17 -0600

    Started to work on my US-11-TC-2

--- src/test/java/com/trycloud/runners/CukesRunner.java
+++ src/test/java/com/trycloud/runners/CukesRunner.java
@@ -13,8 +13,8 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/features",
         glue = "com/trycloud/step_definitions",
-        dryRun = false,
-        tags = "@smoke",
+        dryRun = true,
+        tags = "",
         publish = true
 )
 public class CukesRunner {
