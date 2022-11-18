23f5bf09613855befd106dfa5b871e7fde6ef3d4
Olesya-Talanina <talaninaolesya@gmail.com>
2022-11-17 19:02:46 -0600

    US-11-TC-2 started to work on my second TC

---
+++ src/test/java/com/trycloud/step_definitions/TalkSendMsg_StepDefs.java
@@ -0,0 +1,33 @@
+package com.trycloud.step_definitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class TalkSendMsg_StepDefs {
+    @Given("user on the dashboard page")
+    public void user_on_the_dashboard_page() {
+
+    }
+    @When("the user clicks the {string} module")
+    public void the_user_clicks_the_module(String string) {
+
+    }
+    @And("user search user from the search box")
+    public void user_search_user_from_the_search_box() {
+
+    }
+    @And("user write a message")
+    public void user_write_a_message() {
+
+    }
+    @And("user clicks to submit button")
+    public void user_clicks_to_submit_button() {
+
+    }
+    @Then("the user should be able to see the message is displayed on the conversation log")
+    public void the_user_should_be_able_to_see_the_message_is_displayed_on_the_conversation_log() {
+
+    }
+}
