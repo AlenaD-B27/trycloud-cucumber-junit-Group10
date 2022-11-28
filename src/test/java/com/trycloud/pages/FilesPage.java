d39cde42d4de216154ef80148ed08847dd0a1a47
CumaliBadur <cumalibadur@gmail.com>
2022-11-28 00:39:58 -0500

    FilesPage and Stepdef updated

--- src/test/java/com/trycloud/pages/FilesPage.java
+++ src/test/java/com/trycloud/pages/FilesPage.java
@@ -1,4 +1,25 @@
 package com.trycloud.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class FilesPage extends BasePage {
+
+    @FindBy(xpath = "//*[@id=\"appmenu\"]/li[2]/a")
+    public WebElement fileButton;
+
+    @FindBy(xpath = "//*[@id=\"fileList\"]/tr[1]/td[2]/a/span[2]/a[2]/span[1]")
+    public WebElement actionButton;
+
+    @FindBy(xpath = "//*[@id=\"fileList\"]/tr[1]/td[2]/div/ul/li[4]/a/span[2]")
+    public WebElement detailButton;
+
+    @FindBy(xpath = "//*[@id=\"commentsTabView\"]/span")
+    public WebElement commentsButton;
+
+    @FindBy(xpath = "//*[@id=\"commentsTabView\"]/div[1]/form/div[1]")
+    public WebElement inputComment;
+
+    @FindBy(xpath = "//*[@id=\"commentsTabView\"]/div[1]/form/input")
+    public WebElement sendButton;
 }
--- src/test/java/com/trycloud/step_definitions/WriteCommentsFilesAndFolders.java
+++ src/test/java/com/trycloud/step_definitions/WriteCommentsFilesAndFolders.java
@@ -1,13 +1,17 @@
 package com.trycloud.step_definitions;
 
+import com.trycloud.pages.FilesPage;
 import com.trycloud.pages.LoginPage;
+import com.trycloud.utilities.BrowserUtils;
 import com.trycloud.utilities.ConfigReader;
 import com.trycloud.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.Keys;
 
 public class WriteCommentsFilesAndFolders {
+    FilesPage filesPage = new FilesPage();
 
     @Given("user on the dashboard page")
     public void user_on_the_dashboard_page() {
@@ -17,28 +21,34 @@ public class WriteCommentsFilesAndFolders {
 
     @When("the user clicks the Files module")
     public void the_user_clicks_the_files_module() {
-
+        filesPage.fileButton.click();
+        BrowserUtils.waitFor(1);
 
     }
 
     @When("user click action-icon  from any file on the page")
     public void user_click_action_icon_from_any_file_on_the_page() {
-
+        filesPage.actionButton.click();
+        BrowserUtils.waitFor(1);
     }
 
     @When("user choose the Details option")
     public void user_choose_the_details_option() {
-
+        filesPage.detailButton.click();
+        BrowserUtils.waitFor(1);
     }
 
     @When("user write a comment inside the input box")
     public void user_write_a_comment_inside_the_input_box() {
-
+        filesPage.commentsButton.click();
+        filesPage.inputComment.sendKeys("Life is life" + Keys.ENTER);
+        BrowserUtils.waitFor(1);
     }
 
     @When("user click the submit button to post it")
     public void user_click_the_submit_button_to_post_it() {
-
+        filesPage.sendButton.click();
+        BrowserUtils.waitFor(1);
     }
 
     @Then("Verify the comment is displayed in the comment section.")
